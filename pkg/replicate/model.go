10dd94120cdaa615654a3b1d762f8242e401a662
Mattt Zmuda <mattt@replicate.com>
2023-07-14 04:42:19 -0700

    Extract collection into separate file

--- pkg/replicate/model.go
+++ pkg/replicate/model.go
@@ -1,10 +1,5 @@
 package replicate
 
-import (
-	"context"
-	"fmt"
-)
-
 type Model struct {
 	URL            string        `json:"url"`
 	Owner          string        `json:"owner"`
@@ -25,31 +20,4 @@ type ModelVersion struct {
 	CreatedAt     string      `json:"created_at"`
 	CogVersion    string      `json:"cog_version"`
 	OpenAPISchema interface{} `json:"openapi_schema"`
-}
-
-type Collection struct {
-	Name        string   `json:"name"`
-	Slug        string   `json:"slug"`
-	Description string   `json:"description"`
-	Models      *[]Model `json:"models,omitempty"`
-}
-
-// ListCollections returns a list of all collections.
-func (r *Client) ListCollections(ctx context.Context) (*Page[Collection], error) {
-	response := &Page[Collection]{}
-	err := r.request(ctx, "GET", "/collections", nil, response)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list collections: %w", err)
-	}
-	return response, nil
-}
-
-// GetCollection returns a collection by slug.
-func (r *Client) GetCollection(ctx context.Context, slug string) (*Collection, error) {
-	collection := &Collection{}
-	err := r.request(ctx, "GET", fmt.Sprintf("/collections/%s", slug), nil, collection)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get collection: %w", err)
-	}
-	return collection, nil
 }
