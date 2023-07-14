ef834266af1cd9d78e7d62c077c05885d15c74bd
Mattt Zmuda <mattt@replicate.com>
2023-07-14 04:57:37 -0700

    Refactor request method

--- pkg/replicate/client.go
+++ pkg/replicate/client.go
@@ -41,47 +41,39 @@ func NewClient(auth string, userAgent *string, baseURL *string) *Client {
 }
 
 // request makes an HTTP request to the Replicate API.
-func (r *Client) request(ctx context.Context, method, path string, requestBody interface{}, responseTarget interface{}) error {
-	// Initialize an empty buffer
+func (r *Client) request(ctx context.Context, method, path string, body interface{}, out interface{}) error {
 	bodyBuffer := &bytes.Buffer{}
-
-	// Marshal request body, if provided
-	if requestBody != nil {
-		bodyBytes, err := json.Marshal(requestBody)
+	if body != nil {
+		bodyBytes, err := json.Marshal(body)
 		if err != nil {
 			return fmt.Errorf("failed to marshal request body: %w", err)
 		}
 		bodyBuffer = bytes.NewBuffer(bodyBytes)
 	}
 
-	// Construct request
 	url := constructURL(r.BaseURL, path)
 	request, err := http.NewRequestWithContext(ctx, method, url, bodyBuffer)
 	if err != nil {
 		return fmt.Errorf("failed to create request: %w", err)
 	}
 
-	// Set headers
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Authorization", fmt.Sprintf("Token %s", r.Auth))
 	if r.UserAgent != nil {
 		request.Header.Set("User-Agent", *r.UserAgent)
 	}
 
-	// Send request
 	response, err := r.HTTPClient.Do(request)
 	if err != nil {
 		return fmt.Errorf("failed to make request: %w", err)
 	}
 	defer response.Body.Close()
 
-	// Read response body
 	responseBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return fmt.Errorf("failed to read response body: %w", err)
 	}
 
-	// Check for API errors
 	if response.StatusCode < http.StatusOK || response.StatusCode >= http.StatusBadRequest {
 		var apiError APIError
 		err := json.Unmarshal(responseBytes, &apiError)
@@ -91,9 +83,8 @@ func (r *Client) request(ctx context.Context, method, path string, requestBody i
 		return apiError
 	}
 
-	// Unmarshal response into target, if provided
-	if responseTarget != nil {
-		if err := json.Unmarshal(responseBytes, &responseTarget); err != nil {
+	if out != nil {
+		if err := json.Unmarshal(responseBytes, &out); err != nil {
 			return fmt.Errorf("failed to unmarshal response: %w", err)
 		}
 	}
