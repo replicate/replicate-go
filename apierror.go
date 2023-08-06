78287f58414dc15fb4a6975e87eb61c790608f57
Mattt Zmuda <mattt@replicate.com>
2023-08-06 07:12:22 -0700

    Add convenience function to create APIError

--- apierror.go
+++ apierror.go
@@ -1,12 +1,23 @@
 package replicate
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 // APIError represents an error returned by the Replicate API
 type APIError struct {
 	Detail string `json:"detail"`
+}
+
+func unmarshalAPIError(data []byte) *APIError {
+	apiError := &APIError{}
+	err := json.Unmarshal(data, apiError)
+	if err != nil {
+		apiError.Detail = fmt.Sprintf("Unknown error: %s", err)
+	}
+
+	return apiError
 }
 
 // Error implements the error interface
