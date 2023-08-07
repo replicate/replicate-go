4da00249f788fdaeb6a8bba5763c0c7520456d7c
Mattt Zmuda <mattt@replicate.com>
2023-08-07 06:35:30 -0700

    Add code comments for Backoff symbols

--- backoff.go
+++ backoff.go
@@ -11,22 +11,26 @@ type Backoff interface {
 	NextDelay(retries int) time.Duration
 }
 
+// ConstantBackoff is a backoff strategy that returns a constant delay with some jitter.
 type ConstantBackoff struct {
 	Base   time.Duration
 	Jitter time.Duration
 }
 
+// NextDelay returns the next delay.
 func (b *ConstantBackoff) NextDelay(retries int) time.Duration {
 	jitter := time.Duration(rand.Float64() * float64(b.Jitter))
 	return b.Base + jitter
 }
 
+// ExponentialBackoff is a backoff strategy that returns an exponentially increasing delay with some jitter.
 type ExponentialBackoff struct {
 	Base       time.Duration
 	Multiplier float64
 	Jitter     time.Duration
 }
 
+// NextDelay returns the next delay.
 func (b *ExponentialBackoff) NextDelay(retries int) time.Duration {
 	jitter := time.Duration(rand.Float64() * float64(b.Jitter))
 	return time.Duration(float64(b.Base)*math.Pow(b.Multiplier, float64(retries))) + jitter
