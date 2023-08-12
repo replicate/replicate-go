31e29177f27d7b4917b53112649ad577ad0fe3f1
ShunpeIII <63499912+lll-lll-lll-lll@users.noreply.github.com>
2023-08-12 09:42:28 +0000

    add Download method

--- webhook.go
+++ webhook.go
@@ -1,5 +1,15 @@
 package replicate
 
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
 type Webhook struct {
 	URL    string
 	Events []WebhookEventType
@@ -23,4 +33,61 @@ var WebhookEventAll = []WebhookEventType{
 
 func (w WebhookEventType) String() string {
 	return string(w)
+}
+
+// Download
+// output  is urls of Prediction.Output
+// dirPath is directory for saving images
+func Download(ctx context.Context, output []string, dirPath string) error {
+	errChan := make(chan error)
+	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
+		return err
+	}
+
+	go func() {
+		var wg sync.WaitGroup
+		defer close(errChan)
+		for _, o := range output {
+			wg.Add(1)
+			o := o
+			go func() {
+				defer wg.Done()
+				res, err := http.Get(o)
+				if err != nil {
+					errChan <- err
+					return
+				}
+
+				defer res.Body.Close()
+
+				// if err is nil, return error
+				if _, err := os.Stat(dirPath + "/" + filepath.Base(o)); err == nil {
+					errChan <- fmt.Errorf("%s image file already exists", filepath.Base(o))
+					return
+				}
+
+				out, err := os.Create(dirPath + "/" + filepath.Base(o))
+				if err != nil {
+					errChan <- err
+					return
+				}
+				defer out.Close()
+
+				if _, err := io.Copy(out, res.Body); err != nil {
+					errChan <- err
+					return
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+
+	for {
+		select {
+		case err := <-errChan:
+			return err
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
 }
