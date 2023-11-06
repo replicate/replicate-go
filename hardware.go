b369ed3483fa172f058fc4292b328175d8c0ed24
Mattt <mattt@replicate.com>
2023-11-06 03:57:20 -0800

    Add support for `models.create` and `hardware.list` endpoints (#22)
    * Add support for hardware.list endpoint
    
    * Add support for models.create endpoint

---
+++ hardware.go
@@ -0,0 +1,40 @@
+package replicate
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type Hardware struct {
+	SKU  string `json:"sku"`
+	Name string `json:"name"`
+
+	rawJSON json.RawMessage `json:"-"`
+}
+
+func (h Hardware) MarshalJSON() ([]byte, error) {
+	if h.rawJSON != nil {
+		return h.rawJSON, nil
+	} else {
+		type Alias Hardware
+		return json.Marshal(&struct{ *Alias }{Alias: (*Alias)(&h)})
+	}
+}
+
+func (h *Hardware) UnmarshalJSON(data []byte) error {
+	h.rawJSON = data
+	type Alias Hardware
+	alias := &struct{ *Alias }{Alias: (*Alias)(h)}
+	return json.Unmarshal(data, alias)
+}
+
+// ListHardware returns a list of available hardware.
+func (r *Client) ListHardware(ctx context.Context) (*[]Hardware, error) {
+	response := &[]Hardware{}
+	err := r.request(ctx, "GET", "/hardware", nil, response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list collections: %w", err)
+	}
+	return response, nil
+}
--- replicate_test.go
+++ replicate_test.go
@@ -231,6 +231,75 @@ func TestGetModel(t *testing.T) {
 	assert.Equal(t, "hello-world", model.Name)
 }
 
+func TestCreateModel(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		assert.Equal(t, "/models", r.URL.Path)
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer r.Body.Close()
+
+		var requestBody map[string]interface{}
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "owner", requestBody["owner"])
+		assert.Equal(t, "name", requestBody["name"])
+		assert.Equal(t, "public", requestBody["visibility"])
+		assert.Equal(t, "cpu", requestBody["hardware"])
+
+		response := replicate.Model{
+			Owner:          "owner",
+			Name:           "name",
+			Description:    "",
+			Visibility:     "public",
+			GithubURL:      "",
+			PaperURL:       "",
+			LicenseURL:     "",
+			RunCount:       0,
+			CoverImageURL:  "",
+			DefaultExample: nil,
+			LatestVersion:  nil,
+		}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write(responseBytes)
+	}))
+	defer mockServer.Close()
+
+	client, err := replicate.NewClient(
+		replicate.WithToken("test-token"),
+		replicate.WithBaseURL(mockServer.URL),
+	)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	options := replicate.CreateModelOptions{
+		Visibility: "public",
+		Hardware:   "cpu",
+	}
+	model, err := client.CreateModel(ctx, "owner", "name", options)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "owner", model.Owner)
+	assert.Equal(t, "name", model.Name)
+	assert.Equal(t, "public", model.Visibility)
+	assert.Equal(t, "", model.Description)
+}
+
 func TestListModelVersions(t *testing.T) {
 	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/models/replicate/hello-world/versions", r.URL.Path)
@@ -922,6 +991,48 @@ func TestListTrainings(t *testing.T) {
 	assert.Len(t, page.Results, 2)
 	assert.Equal(t, "ufawqhfynnddngldkgtslldrkq", page.Results[0].ID)
 	assert.Equal(t, "rrr4z55ocneqzikepnug6xezpe", page.Results[1].ID)
+}
+
+func TestListHardware(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/hardware", r.URL.Path)
+		assert.Equal(t, http.MethodGet, r.Method)
+
+		response := []replicate.Hardware{
+			{Name: "CPU", SKU: "cpu"},
+			{Name: "Nvidia T4 GPU", SKU: "gpu-t4"},
+			{Name: "Nvidia A40 GPU", SKU: "gpu-a40-small"},
+			{Name: "Nvidia A40 (Large) GPU", SKU: "gpu-a40-large"},
+		}
+
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseBytes)
+	}))
+	defer mockServer.Close()
+
+	client, err := replicate.NewClient(
+		replicate.WithToken("test-token"),
+		replicate.WithBaseURL(mockServer.URL),
+	)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	hardwareList, err := client.ListHardware(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 4, len(*hardwareList))
+
+	assert.Equal(t, "CPU", (*hardwareList)[0].Name)
+	assert.Equal(t, "cpu", (*hardwareList)[0].SKU)
 }
 
 func TestAutomaticallyRetryGetRequests(t *testing.T) {
