95eec8124486aa57ef694ca41d71787550fb890e
Mattt <mattt@replicate.com>
2023-12-04 04:23:19 -0800

    Improve handling of stream termination and add reconnect mechanism (#33)
    * Select done in line processing goroutine
    
    This should help ensure that streams terminate correctly
    
    * Conditionally send Last-Event-ID header
    
    * Attempt to reconnect if response is closed before done event
    
    Correctly handle done events in goroutine
    
    * Don't emit error when context is canceled

--- stream.go
+++ stream.go
@@ -80,44 +80,52 @@ func (r *Client) Stream(ctx context.Context, identifier string, input Prediction
 		return sseChan, errChan
 	}
 
-	return r.streamPrediction(ctx, prediction, sseChan, errChan)
+	r.streamPrediction(ctx, prediction, nil, sseChan, errChan)
+
+	return sseChan, errChan
 }
 
 func (r *Client) StreamPrediction(ctx context.Context, prediction *Prediction) (<-chan SSEEvent, <-chan error) {
 	sseChan := make(chan SSEEvent, 64)
 	errChan := make(chan error, 64)
 
-	return r.streamPrediction(ctx, prediction, sseChan, errChan)
+	r.streamPrediction(ctx, prediction, nil, sseChan, errChan)
+
+	return sseChan, errChan
 }
 
-func (r *Client) streamPrediction(ctx context.Context, prediction *Prediction, sseChan chan SSEEvent, errChan chan error) (<-chan SSEEvent, <-chan error) {
+func (r *Client) streamPrediction(ctx context.Context, prediction *Prediction, lastEvent *SSEEvent, sseChan chan SSEEvent, errChan chan error) {
 	g, ctx := errgroup.WithContext(ctx)
 
 	url := prediction.URLs["stream"]
 	if url == "" {
-		errChan <- errors.New("streaming not supported")
-		return sseChan, errChan
+		errChan <- errors.New("streaming not supported or not enabled for this prediction")
+		return
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		errChan <- fmt.Errorf("failed to create request: %w", err)
-		return sseChan, errChan
+		return
 	}
 	req.Header.Set("Accept", "text/event-stream")
 	req.Header.Set("Cache-Control", "no-cache")
 	req.Header.Set("Connection", "keep-alive")
 
+	if lastEvent != nil {
+		req.Header.Set("Last-Event-ID", lastEvent.ID)
+	}
+
 	resp, err := r.c.Do(req)
 	if err != nil {
 		resp.Body.Close()
 		errChan <- fmt.Errorf("failed to send request: %w", err)
-		return sseChan, errChan
+		return
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		errChan <- fmt.Errorf("received invalid status code %d", resp.StatusCode)
-		return sseChan, errChan
+		errChan <- fmt.Errorf("received invalid status code: %d", resp.StatusCode)
+		return
 	}
 
 	done := make(chan struct{})
@@ -131,15 +139,14 @@ func (r *Client) streamPrediction(ctx context.Context, prediction *Prediction, s
 
 		for {
 			select {
+			case <-ctx.Done():
+				return nil
 			case <-done:
 				return nil
 			default:
 				line, err := reader.ReadBytes('\n')
 				if err != nil {
 					defer resp.Body.Close()
-					if err == io.EOF {
-						return nil
-					}
 					return err
 				}
 				lineChan <- line
@@ -151,6 +158,8 @@ func (r *Client) streamPrediction(ctx context.Context, prediction *Prediction, s
 		for {
 			select {
 			case <-ctx.Done():
+				return
+			case <-done:
 				return
 			case b, ok := <-lineChan:
 				if !ok {
@@ -186,11 +195,28 @@ func (r *Client) streamPrediction(ctx context.Context, prediction *Prediction, s
 		defer close(sseChan)
 		defer close(errChan)
 
-		err := g.Wait()
-		if err != nil {
-			errChan <- err
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-done:
+				return
+			default:
+				err := g.Wait()
+				if err != nil {
+					if err == io.EOF {
+						// Attempt to reconnect if the connection was closed before the stream was done
+						r.streamPrediction(ctx, prediction, lastEvent, sseChan, errChan)
+						continue
+					}
+
+					if errors.Is(err, context.Canceled) {
+						return
+					}
+
+					errChan <- err
+				}
+			}
 		}
 	}()
-
-	return sseChan, errChan
 }
