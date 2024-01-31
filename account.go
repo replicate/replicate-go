4a2b121de555ecc392252a8d232add0ec5bc1772
Mattt <mattt@replicate.com>
2024-01-31 06:01:33 -0800

    Add support for accounts.current endpoint (#37)

---
+++ account.go
@@ -0,0 +1,42 @@
+package replicate
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type Account struct {
+	Type      string `json:"type"`
+	Username  string `json:"username"`
+	Name      string `json:"name"`
+	GithubURL string `json:"github_url"`
+
+	rawJSON json.RawMessage `json:"-"`
+}
+
+func (a Account) MarshalJSON() ([]byte, error) {
+	if a.rawJSON != nil {
+		return a.rawJSON, nil
+	} else {
+		type Alias Account
+		return json.Marshal(&struct{ *Alias }{Alias: (*Alias)(&a)})
+	}
+}
+
+func (a *Account) UnmarshalJSON(data []byte) error {
+	a.rawJSON = data
+	type Alias Account
+	alias := &struct{ *Alias }{Alias: (*Alias)(a)}
+	return json.Unmarshal(data, alias)
+}
+
+// GetCurrentAccount returns the authenticated user or organization.
+func (r *Client) GetCurrentAccount(ctx context.Context) (*Account, error) {
+	response := &Account{}
+	err := r.fetch(ctx, "GET", "/account", nil, response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list collections: %w", err)
+	}
+	return response, nil
+}
